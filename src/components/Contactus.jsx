f62d910b6cb198eb672d1bc8ad50eb75190b748e
salimzayd <zidsalim786@gmail.com>
2024-05-16 09:23:51 +0530

    th

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import { IoLogoInstagram } from "react-icons/io";
 import { FaWhatsapp } from "react-icons/fa";
+import { CiLocationOn } from "react-icons/ci";
+import { FaPhoneAlt } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+
 
 import '../components/Contactus.css'
 
 
 const Contactus = () => {
+
+  const contactdetails = [
+    {
+    id:1,
+    icon:<CiLocationOn />,
+    head:"Our Location :",
+    details:"SoHo 94 Broadway st new york,NY 1001"
+
+  },
+  {
+    id:2,
+    icon:<FaPhoneAlt />,
+    head:"Our Phonenumber :",
+    details:"+1 78456123564,+01 7856289658"
+
+  },
+  {
+    id:3,
+    icon:<MdEmail />,
+    head:"Our Email",
+    details:"FoodFleet301@gmail.com"
+
+  }
+]
   return (
 
     <div className='main-cntct'>
@@ -39,7 +67,31 @@ const Contactus = () => {
       <h2 className='caption'>Culinary convenience at your fingertips! Experience the epitome of gastronomic delight with our seamless food delivery app. From delectable dishes to doorstep satisfaction, indulge in a culinary journey like never before.</h2>
       </div>
     </div>
+
+    <div className='main-cn'>
+            {contactdetails.map((item) =>(
+            <>
+            <div className='cards'>
+              <div>
+              <div className='cr-icon'>{item.icon}</div>    
+              </div>
+            
+              <div  className='cr-contents'>
+                <h2>{item.head}</h2>
+                <h4>{item.details}</h4>
+              </div>
+            </div>
+              
+            </>
+        
+        
+          
+            ))}
     </div>
+</div>
+  
+
+    
      )
 }
 
